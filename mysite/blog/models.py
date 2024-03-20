b5410bea34474bb0322225472296d6a4cf2a8a06
Israel Colín <israaacolin@gmail.com>
2024-03-20 09:57:19 -0600

    Complemento 1er commit

---
+++ mysite/blog/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    # Llave foránea
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='blog_posts'
+    )
+    body = models.TextField()
+    # Por defecto la fecha actual
+    publish = models.DateTimeField(default=timezone.now)
+    # Se agrega fecha actual al crear
+    created = models.DateTimeField(auto_now_add=True)
+    # Se actualiza a fecha actual con update
+    updated = models.DateTimeField(auto_now=True)
+    # Opciones de estado
+    status = models.CharField(
+        max_length=2,
+        choices=Status.choices,
+        default=Status.DRAFT
+    )
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish'])
+        ]
+
+    def __str__(self):
+        return self.title
